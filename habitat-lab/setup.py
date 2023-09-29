18c2bce0f997cf656a682d5aca4560d3d4eeb337
Chris Klammer <christopher.klammer@gmail.com>
2023-09-29 17:20:32 -0400

    Update setup.py

--- habitat-lab/setup.py
+++ habitat-lab/setup.py
@@ -39,7 +39,7 @@ PROJECT_URLS = {
 EXTRA_DEPS = {
     # Home robot dependency for velocity controllers
     "home_robot": [
-        "home-robot @ git+ssh://git@github.com/facebookresearch/home-robot.git@habitat-challenge-2023#subdirectory=src/home_robot",
+        "home-robot @ https://github.com/facebookresearch/home-robot.git@habitat-challenge-2023#subdirectory=src/home_robot",
     ]
 }
 
